fb7f4a4a8b919c720cf8830762a27d59c3aeb4cb
George Yanev <gyanev@coursera.org>
2021-01-20 12:53:14 +0200

    Missing variables required by the IAM role

--- variables.tf
+++ variables.tf
@@ -13,3 +13,27 @@ variable "policy" {
   type        = string
   description = "Policy for the IAM Role"
 }
+
+variable "path" {
+  default     = "/"
+  type        = string
+  description = "Path in which to create the role and the policy."
+}
+
+variable "description" {
+  default     = "Managed by Terraform"
+  type        = string
+  description = "The description of the role and the policy."
+}
+
+variable "max_session_duration" {
+  default     = "3600"
+  type        = string
+  description = "The maximum session duration (in seconds) that you want to set for the specified role."
+}
+
+variable "force_detach_policies" {
+  default     = false
+  type        = bool
+  description = "Specifies to force detaching any policies the role has before destroying it."
+}
